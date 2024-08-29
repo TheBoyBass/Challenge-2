1a05f9af654c2037a8a5d8e692228693cdd53b31
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-08-29 06:04:42 +0200

    Updated personal files

---
+++ ChallengeE/alex.js
@@ -0,0 +1,6 @@
+let firstname = "Alex"
+let surname = "Naidoo"
+let role = "Head of Marketing"
+
+let display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display
---
+++ ChallengeE/nwabisa.js
@@ -0,0 +1,6 @@
+let firstname = "Nwabisa"
+let surname = "Gabe"
+let role = "CEO"
+
+let display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#nwabisa').innerText = display
